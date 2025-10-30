25b3fafcc86e5254340bac7f0bc840d5df37dceb
boeun <supbe0726@naver.com>
2025-10-31 00:29:24 +0900

    css -> modal.css로 수정

--- src/components/list/Item.jsx
+++ src/components/list/Item.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 import itemImg from "../../assets/img/item.png";
 import wishiIcon from "../../assets/img/wish.svg";
 import wishiActiveIcon from "../../assets/img/wish_active.svg";
-import "../../assets/css/item.css"
+import styles from "../../assets/css/item.module.css";
 
 function Item(props) {
-    return (
-        <div className='prd_item'>
-            <div className="img_box">
-                <div className="event">1 +  1</div>
-                <img src={itemImg} alt="" />
-                {/* <button type='button' className='wish_btn'><img src={wishiIcon} alt="" /></button> */}
-                <button type='button' className='wish_btn'><img src={wishiActiveIcon} alt="" /></button>
-            </div>
-            <div className="info_box">
-                <span className="category cu">CU</span>
-                <p className="title">서울우유 강릉커피</p>
-                <p className="price">2,500원</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.prd_item}>
+      <div className={styles.img_box}>
+        <div className={styles.event}>1 + 1</div>
+        <img src={itemImg} alt="" />
+        {/* <button type='button' className='wish_btn'><img src={wishiIcon} alt="" /></button> */}
+        <button type="button" className={styles.wish_btn}>
+          <img src={wishiActiveIcon} alt="" />
+        </button>
+      </div>
+      <div className={styles.info_box}>
+        <span className={styles.category.cu}>CU</span>
+        <p className={styles.title}>서울우유 강릉커피</p>
+        <p className={styles.price}>2,500원</p>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
